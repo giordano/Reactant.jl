fa4999277452dc89db96f5fb3f6fed532bfe4374
William Moses <gh@wsmoses.com>
2025-02-21 18:08:23 -0600

    Further enzymexla bump (#785)
    * Further enzymexla bump
    
    * more enzymejax bump
    
    * Update src/Compiler.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- deps/ReactantExtra/API.cpp
+++ deps/ReactantExtra/API.cpp
@@ -133,15 +133,10 @@ extern "C" void (*ReactantThrowError)(const char *) = nullptr;
 
 // Utilities for `StatusOr`.
 template <typename T> T MyValueOrThrow(absl::StatusOr<T> v) {
-  if (ReactantThrowError) {
     if (!v.ok()) {
       ReactantThrowError(v.status().ToString().c_str());
-      throw xla::XlaRuntimeError(v.status().ToString().c_str());
     }
     return std::move(v).value();
-  } else {
-    return xla::ValueOrThrow(std::move(v));
-  }
 }
 
 extern "C" void ReactantHandleCuResult(uint32_t curesult) {
